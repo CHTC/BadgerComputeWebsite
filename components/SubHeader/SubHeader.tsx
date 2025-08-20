eb738cf288be1b1cd53023359ee3df2d13a4e804
Cannon Lock <clock@wisc.edu>
2025-08-20 09:38:16 -0500

    Add material icons

--- components/SubHeader/SubHeader.tsx
+++ components/SubHeader/SubHeader.tsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import {ReactNode} from "react";
-import {Grid} from "@mui/system";
+import {Grid} from "@mui/material/Grid";
 
 interface SubHeaderProps {
 	title?: ReactNode;
