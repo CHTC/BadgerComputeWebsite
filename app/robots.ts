cedf85c3bea4a26b03e88e75da2564dc982463ae
Cannon Lock <clock@wisc.edu>
2025-09-02 10:40:26 -0500

    Add Robots and Sitemap

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: {
+			allow: ['/'],
+			disallow: '/docs/admin-docs/',
+		},
+		sitemap: "https://badgercompute.wisc.edu/sitemap.xml",
+		host: "https://badgercompute.wisc.edu"
+	}
+}
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://badgercompute.wisc.edu",
+	output: "export"
+}
