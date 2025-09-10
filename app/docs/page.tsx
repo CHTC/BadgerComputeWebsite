4e53a3ea52e3cb9f6edecf3dbc4521188cb96fb8
Cannon Lock <clock@wisc.edu>
2025-09-10 14:45:35 -0500

    Allow nested documentation

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,23 +1,63 @@
-import { Typography, Grid, Link } from "@mui/material";
+import { Typography, Grid, Link, TypographyVariant } from "@mui/material";
+import {ElementType, cloneElement, ReactElement} from "react";
+
+
 import { navigation } from "./navigation";
 import DocumentationCard from "@/components/DocumentationCard";
 
+import type { NavigationItem } from "@/components/DocumentationSidebar";
+
+const recursableNavigation = {
+	label: "BadgerCompute Documentation",
+	children: navigation
+}
+
 const Page = () => {
+	return <Grid container spacing={2}>
+		<NavigationItem {...recursableNavigation} depth={1} />
+	</Grid>
+};
+
+const NavigationItem = ({label, path, icon, children, depth}: NavigationItem & {depth: number}) => {
+
+	// Toss an error if the depth is too high
+	// This allows us to cast the types below safely
+	if(depth > 5) {
+		throw new Error("NavigationItem depth exceeds maximum of 5");
+	}
+
+	if(!children) {
+		return (
+			<Grid display={'flex'} size={{xs: 12, md: 6}}>
+				<Link href={path} style={{textDecoration: 'none', width: '100%'}}>
+					<DocumentationCard icon={icon} title={label} />
+				</Link>
+			</Grid>
+		)
+	}
 
 	return (
 		<>
-			<Typography component={'h1'} variant="h2" gutterBottom>BadgerCompute Documentation</Typography>
-			<Grid container spacing={2}>
-				{navigation.map(navItem => (
-					<Grid display={'flex'} size={{xs: 12, md: 6, lg: 4}} key={navItem.label}>
-						<Link href={navItem.path} style={{textDecoration: 'none', width: '100%'}}>
-							<DocumentationCard icon={navItem.icon} title={navItem.label} />
-						</Link>
-					</Grid>
-				))}
+			<Grid size={12}>
+				<Typography
+					component={`h${depth}` as ElementType}
+					variant={`h${depth + 1}` as TypographyVariant}
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+					}}
+					gutterBottom
+				>
+					{icon && cloneElement(icon as ReactElement, {style: {fontSize: 'inherit', marginRight: '1rem'}})}
+					{label}
+				</Typography>
 			</Grid>
+			{children.map(child => (
+				<NavigationItem key={child.label} depth={depth + 1} {...child} />
+			))}
 		</>
-	);
-};
+	)
+
+}
 
 export default Page;
